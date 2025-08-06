0114e41fdc664cd9e345903fa82d5a141cf459fb
Marek <markur453@gmail.com>
2025-08-06 20:10:58 +0600

    update

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("playButton").addEventListener("click", function() {
+    document.getElementById("ButtonSFX").play();
+});
+
+document.getElementById("playButton1").addEventListener("click", function() {
+    document.getElementById("ButtonSFX").play();
+});
+
+
+const bgMusic = document.getElementById("Theme");
+bgMusic.play();
